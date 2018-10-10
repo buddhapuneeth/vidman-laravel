da0aa864e8272a0326eba86d49ba6994a435b613
Buddha Puneeth <buddhapuneeth@gmail.com>
2018-10-09 18:37:10 -0700

    128 did not work. reverting to 256

--- config/app.php
+++ config/app.php
@@ -78,9 +78,9 @@ return [
 	|
 	*/
 
-	'key' => env('APP_KEY', 'SomeRandomString'),
+	'key' => env('APP_KEY', 'SomeRandomStringWith32Characters'),
 
-	'cipher' => 'AES-128-CBC',
+	'cipher' => 'AES-256-CBC',
 
 	/*
 	|--------------------------------------------------------------------------
